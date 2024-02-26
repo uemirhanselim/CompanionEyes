59bc58725b39b0f173ae3a6b5193020f99ea7871
uemirhanselim <uemirhanselim@gmail.com>
2024-02-26 11:53:29 +0300

    colors added

--- lib/src/utils/constants.dart
+++ lib/src/utils/constants.dart
@@ -5,3 +5,8 @@ import 'package:flutter/material.dart';
 const Color kWhite = Color(0xFFFFFFFF);
 const Color kBlack = Color(0xFF000000);
 const Color kTransparent = Colors.transparent;
+const Color kSaltwaterDenim = Color(0xff195F7A);
+const Color kBlackOut = Color(0xff222222);
+const Color kFormalGrey = Color(0xff97979A);
+const Color kPlaster = Color(0xffEAE9EA);
+const Color kAluminumSilver = Color(0xff8E8D92);
