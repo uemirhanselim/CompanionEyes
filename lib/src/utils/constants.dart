cde1328ae367e02a3f268a500ba3da57546abe95
uemirhanselim <uemirhanselim@gmail.com>
2024-02-26 10:56:17 +0300

    constants file created

---
+++ lib/src/utils/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+// --------------------- COLORS ---------------------
+
+const Color kWhite = Color(0xFFFFFFFF);
+const Color kBlack = Color(0xFF000000);
+const Color kTransparent = Colors.transparent;
