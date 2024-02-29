575678f6e06e929b603da36c2ff73415f81d4299
uemirhanselim <uemirhanselim@gmail.com>
2024-02-29 17:13:15 +0300

    privacy and terms viewmodel created

---
+++ lib/src/viewmodel/auth/privacy_and_terms_viewmodel.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PrivacyAndTermsViewModel extends ChangeNotifier {
+  PrivacyAndTermsViewModel() {
+// Add your initialization code here
+  }
+// Setters
+// Getters
+// Other methods
+}
