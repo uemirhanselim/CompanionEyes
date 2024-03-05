6a50ab2d1ea9f1de7add6fea3dc69b7d6deec4a9
uemirhanselim <uemirhanselim@gmail.com>
2024-03-05 12:57:50 +0300

    settings viewmodel created

---
+++ lib/src/viewmodel/settings/settings_viewmodel.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SettingsViewModel extends ChangeNotifier {
+  SettingsViewModel() {
+// Add your initialization code here
+  }
+// Setters
+// Getters
+// Other methods
+}
