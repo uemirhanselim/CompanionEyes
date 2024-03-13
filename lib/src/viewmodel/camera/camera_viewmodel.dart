0e03f42a3b8afb070ae813c79573f8bca435c7ec
uemirhanselim <uemirhanselim@gmail.com>
2024-03-13 13:35:52 +0300

    camera  viewmodel update

---
+++ lib/src/viewmodel/camera/camera_viewmodel.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class CameraViewModel extends ChangeNotifier {
+CameraViewModel() {
+// Add your initialization code here
+}
+// Setters
+// Getters
+// Other methods
+}
