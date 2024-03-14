0d72b7d44cd7ed78c7619650b39673ac016469a6
uemirhanselim <uemirhanselim@gmail.com>
2024-03-14 11:22:09 +0300

    forgot password files fixed

--- lib/src/view/auth/forgot_password_view.dart
+++ lib/src/view/auth/forgot_password_view.dart
@@ -1,5 +1,4 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:companioneyes/src/routes/app_router.dart';
 import 'package:companioneyes/src/utils/ui_helper.dart';
 import 'package:companioneyes/src/view/widgets/back_app_bar.dart';
 import 'package:companioneyes/src/view/widgets/shared_button.dart';
@@ -7,7 +6,6 @@ import 'package:companioneyes/src/view/widgets/shared_text_form_field.dart';
 import 'package:companioneyes/src/viewmodel/auth/forgot_password_viewmodel.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
 @RoutePage()
 class ForgotPasswordView extends StatelessWidget {
@@ -15,60 +13,41 @@ class ForgotPasswordView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-create: (_) {
-  final viewModel = ForgotPasswordViewModel();
-  viewModel.init();
-  return viewModel;
-},
-      child: Scaffold(
-        appBar: const BackAppBar(),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: UIHelper.pagePadding(context),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                UIHelper.emptySpaceHeight(context, 0.02),
-                _headlineText(context),
-                UIHelper.emptySpaceHeight(context, 0.05),
-                _fields(context),
-                UIHelper.emptySpaceHeight(context, 0.3),
-                _button(context),
-                UIHelper.emptySpaceHeight(context, 0.04),
-              ],
+      create: (_) => ForgotPasswordViewModel(),
+      child:
+          Consumer<ForgotPasswordViewModel>(builder: (context, viewModel, _) {
+        return Scaffold(
+          appBar: const BackAppBar(),
+          body: SingleChildScrollView(
+            child: Padding(
+              padding: UIHelper.pagePadding(context),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  UIHelper.emptySpaceHeight(context, 0.02),
+                  _headlineText(context),
+                  UIHelper.emptySpaceHeight(context, 0.05),
+                  _fields(context),
+                  UIHelper.emptySpaceHeight(context, 0.3),
+                  _button(context, viewModel),
+                  UIHelper.emptySpaceHeight(context, 0.04),
+                ],
+              ),
             ),
           ),
-        ),
-      ),
+        );
+      }),
     );
   }
 
-  Widget _button(BuildContext context) =>
-    Consumer<ForgotPasswordViewModel>(builder: (context, viewModel, _) {
-  return SharedButton(
-    color: UIHelper.black,
-    title: "Reset Password",
-    onPressed: viewModel.isResetButtonActive
-        ? () async {
-            try {
-              await FirebaseAuth.instance.sendPasswordResetEmail(
-                  email: viewModel.emailController.text.trim());
-              ScaffoldMessenger.of(context).showSnackBar(
-                const SnackBar(
-                  content: Text('Password reset email sent successfully'),
-                ),
-              );
-            } catch (e) {
-              ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(
-                  content: Text('Failed to send password reset email: $e'),
-                ),
-              );
-            }
-          }
-        : null,
-  );
-});
+  Widget _button(BuildContext context, ForgotPasswordViewModel viewModel) =>
+      SharedButton(
+        color: UIHelper.black,
+        title: "Reset Password",
+        onPressed: viewModel.isResetButtonActive
+            ? () async => viewModel.passwordResetEmailSend(context)
+            : null,
+      );
 
   Widget _fields(BuildContext context) => Consumer<ForgotPasswordViewModel>(
         builder: (context, viewModel, _) {
