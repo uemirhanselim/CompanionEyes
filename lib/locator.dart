26146125932eef3e2c7f9bd8868abb63cf06e4a1
uemirhanselim <uemirhanselim@gmail.com>
2024-03-14 14:39:15 +0300

    authService DI done

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,3 +1,4 @@
+import 'package:companioneyes/src/service/auth/auth_service.dart';
 import 'package:get_it/get_it.dart';
 
 GetIt locator = GetIt.instance;
@@ -5,4 +6,6 @@ GetIt locator = GetIt.instance;
 void initLocator() {
   // ----------------- Example Usage -----------------
   // locator.registerLazySingleton(() => AuthService());
+
+  locator.registerLazySingleton(() => AuthService());
 }
