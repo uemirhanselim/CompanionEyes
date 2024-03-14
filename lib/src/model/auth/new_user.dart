2b2be64c55f2992852ea9afb9e33a191fad55189
uemirhanselim <uemirhanselim@gmail.com>
2024-03-14 14:39:25 +0300

    new user model created

---
+++ lib/src/model/auth/new_user.dart
@@ -0,0 +1,19 @@
+class NewUser {
+  final String email;
+  final String password;
+  final String firstName;
+  final String lastName;
+  final String gender;
+  final String language;
+  final bool isVolunteer;
+
+  NewUser({
+    required this.email,
+    required this.password,
+    required this.firstName,
+    required this.lastName,
+    required this.gender,
+    required this.language,
+    required this.isVolunteer,
+  });
+}
